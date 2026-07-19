a706870908f382d4e28f2f93d60eb118f763444e
Sebastião Morais <gerenciamorais@gmail.com>
2026-07-18 22:31:07 -0300

    Melhora legibilidade dos itens do menu lateral
    Aumenta tamanho/peso da fonte e clareia a cor padrao (nao ativo) dos itens do menu, que estavam finos/apagados demais no fundo escuro.

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 <link rel="stylesheet" type="text/css" href="assets/css/theme/blue.css">
 <link rel="stylesheet" type="text/css" href="assets/css/theme/red.css">
 <link rel="stylesheet" type="text/css" href="assets/css/theme/yellow.css">
-<link rel="stylesheet" type="text/css" href="assets/css/admin-modern.css?v=2.1.0">
+<link rel="stylesheet" type="text/css" href="assets/css/admin-modern.css?v=2.1.1">
 
 </head>
 <body class="login-shell">
